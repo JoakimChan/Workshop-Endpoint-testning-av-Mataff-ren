d3de5d9c9678c643d41c180681249a0e62645175
JoakimChan <120197963+JoakimChan@users.noreply.github.com>
2024-12-12 04:55:17 +0100

    commit
    working makePrecompile.js

--- makePrecompile.js
+++ makePrecompile.js
@@ -1,23 +1,27 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-// Convert Scenario Outlines with  | {dynamic: true, max: X} |
-// or just | {dynamic: true} |
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Convert Scenario Outlines with | {dynamic: true, max: X} | or just | {dynamic: true} |
 // into scenario outlines Cucumber understands
-fs.rmSync(path.join(import.meta.dirname, '__temp'), { recursive: true, force: true });
-fs.mkdirSync(path.join(import.meta.dirname, '__temp'));
-fs
-  .readdirSync(path.join(import.meta.dirname, 'tests', 'features'), { recursive: true })
+fs.rmSync(path.join(__dirname, '__temp'), { recursive: true, force: true });
+fs.mkdirSync(path.join(__dirname, '__temp'));
+
+fs.readdirSync(path.join(__dirname, 'tests', 'features'), { recursive: true })
   .filter(x => x.endsWith('.feature'))
   .forEach(fileName => {
-    let content = fs.readFileSync(path.join(import.meta.dirname, 'tests', 'features', fileName), 'utf-8');
+    let content = fs.readFileSync(path.join(__dirname, 'tests', 'features', fileName), 'utf-8');
     content = content.replace(/\n\s*\|\s*\{dynamic[^\|]*\|/g, toReplace => {
       let details;
       try {
         let func = new Function(`return ${toReplace.split('|')[1]}`);
         details = func();
+      } catch (e) {
+        console.log(e);
+        return '';
       }
-      catch (e) { console.log(e); return ''; }
       if (details.dynamic) {
         details.max = isNaN(details.max) ? 1000 : details.max;
         let indent = ''.padEnd(toReplace.split('|')[0].length, ' ');
@@ -27,5 +31,5 @@ fs
       }
       return '';
     });
-    fs.writeFileSync(path.join(import.meta.dirname, '__temp', fileName), content, 'utf-8');
+    fs.writeFileSync(path.join(__dirname, '__temp', fileName), content, 'utf-8');
   });
